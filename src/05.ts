e1f050b0ab882687dbfb0505d9fcf58d89b4b378
Brandon Mitchell <brandon.m.mitchell@gmail.com>
2025-11-20 13:11:34 +0000

    Day 5 Part 2

--- src/05.ts
+++ src/05.ts
@@ -1,5 +1,6 @@
 import { open } from "node:fs/promises";
 
+// Part 1
 const VOWELS = ["a", "e", "i", "o", "u"];
 const NAUGHTY_STRINGS = ["ab", "cd", "pq", "xy"];
 
@@ -11,11 +12,11 @@ export function hasNaughtyString(str: string): boolean {
   return NAUGHTY_STRINGS.some((ns) => str.includes(ns));
 }
 
-export function hasRepeatingLetters(str: string): boolean {
+export function hasRepeatingLetters(str: string, gap = 0): boolean {
   return (
     [...str].find((value, index, obj) => {
-      if (index < obj.length - 1) {
-        return value === obj[index + 1];
+      if (index < obj.length - (1 + gap)) {
+        return value === obj[index + (1 + gap)];
       }
 
       return false;
@@ -37,6 +38,83 @@ export async function getTotalNiceStringsCount(): Promise<number> {
   const file = await open("./src/input/05.txt");
   for await (const str of file.readLines()) {
     if (isNiceString(str)) {
+      niceStringCount++;
+    }
+  }
+
+  return niceStringCount;
+}
+
+// Part 2
+export function hasRepeatingLettersTwice(str: string): boolean {
+  const pairStore = new Map<string, Set<number>>();
+
+  [...str].forEach((value, index, array) => {
+    if (index < array.length - 1) {
+      const key = `${value}${array[index + 1]}`;
+      const values = pairStore.get(key);
+
+      if (!values) {
+        pairStore.set(key, new Set([index, index + 1]));
+      } else {
+        values.add(index);
+        values.add(index + 1);
+        pairStore.set(key, values);
+      }
+    }
+  });
+
+  const candidates = Array.from(pairStore.values()).filter(
+    (values) => values.size >= 4
+  );
+
+  return (
+    candidates.find((c) => findSequentialEntries([...c]) >= 2) !== undefined
+  );
+}
+
+/**
+ *
+ * @param arr - an array of numbers
+ * @param pairs - the number of non-overlapping sequential pairs (starts with zero)
+ * @returns number of non-overlapping sequential pairs in the array
+ */
+export function findSequentialEntries(arr: number[], pairs = 0) {
+  const pairIdx: number[] = [];
+
+  arr.forEach((value, index, array) => {
+    if (index < array.length - 1) {
+      const match = value + 1 === array[index + 1];
+
+      if (match) {
+        pairIdx.push(index);
+        pairIdx.push(index + 1);
+      }
+    }
+  });
+
+  if (pairIdx.length >= 2 && typeof pairIdx[0] === "number") {
+    pairs++;
+    arr.splice(pairIdx[0], 2);
+    return findSequentialEntries(arr, pairs);
+  }
+
+  return pairs;
+}
+
+export function isRevisedNiceString(str: string): boolean {
+  if (!hasRepeatingLetters(str, 1)) return false;
+  if (hasRepeatingLettersTwice(str)) return true;
+
+  return false;
+}
+
+export async function getTotalNiceStringsCountPartTwo(): Promise<number> {
+  let niceStringCount = 0;
+
+  const file = await open("./src/input/05.txt");
+  for await (const str of file.readLines()) {
+    if (isRevisedNiceString(str)) {
       niceStringCount++;
     }
   }
--- src/__test__/05.test.ts
+++ src/__test__/05.test.ts
@@ -1,8 +1,12 @@
 import { describe, it } from "node:test";
 import {
+  findSequentialEntries,
   getTotalNiceStringsCount,
+  getTotalNiceStringsCountPartTwo,
   hasNaughtyString,
   hasRepeatingLetters,
+  hasRepeatingLettersTwice,
+  isRevisedNiceString,
   vowelCount,
 } from "../05.js";
 import assert from "node:assert";
@@ -26,13 +30,18 @@ describe("hasNaughtyString", () => {
 });
 
 describe("hasRepeatingLetters", () => {
-  it("contains repeating letters", () => {
+  it("detects repeating letters", () => {
     assert.ok(hasRepeatingLetters("ugknbfddgicrmopn"));
     assert.ok(hasRepeatingLetters("aaa"));
+    assert.ok(!hasRepeatingLetters("jchzalrnumimnmhp"));
   });
 
-  it("does not contain repeating letters", () => {
-    assert.ok(!hasRepeatingLetters("jchzalrnumimnmhp"));
+  it("detects repeating letters, separated by a specified gap size", () => {
+    assert.ok(hasRepeatingLetters("xyx", 1));
+    assert.ok(hasRepeatingLetters("abcdefeghi", 1));
+    assert.ok(hasRepeatingLetters("aaa", 1));
+
+    assert.ok(!hasRepeatingLetters("uurcxstgmygtbstg", 1));
   });
 });
 
@@ -41,5 +50,46 @@ describe("getTotalNiceStringsCount", () => {
     const numNiceStrings = await getTotalNiceStringsCount();
 
     assert.equal(numNiceStrings, 255);
+  });
+});
+
+describe("findPairsByIndex", () => {
+  it("finds number of occurrences of sequential numbers in an array, without overlapping", () => {
+    assert.equal(findSequentialEntries([0, 1, 3, 12, 13, 20]), 2);
+    assert.equal(findSequentialEntries([0, 1, 2, 3, 4]), 2);
+    assert.equal(findSequentialEntries([0, 1, 2]), 1);
+    assert.equal(
+      findSequentialEntries([0, 1, 2, 3, 20, 21, 22, 30, 31, 32, 33]),
+      5
+    );
+  });
+});
+
+describe("hasRepeatingLettersTwice", () => {
+  it("checks if a string has a pair of any two letters that appear at least twice without overlapping", () => {
+    assert.ok(hasRepeatingLettersTwice("xyxy"));
+    assert.ok(hasRepeatingLettersTwice("aabcdefgaa"));
+    assert.ok(hasRepeatingLettersTwice("qjhvhtzxzqqjkmpb"));
+    assert.ok(hasRepeatingLettersTwice("xxyxx"));
+
+    assert.ok(!hasRepeatingLettersTwice("aaa"));
+    assert.ok(!hasRepeatingLettersTwice("ieodomkazucvgmuy"));
+  });
+});
+
+describe("isRevisedNiceString", () => {
+  it("checks if a string meetings the revised criteria for a 'nice string'", () => {
+    assert.ok(isRevisedNiceString("qjhvhtzxzqqjkmpb"));
+    assert.ok(isRevisedNiceString("xxyxx"));
+    assert.ok(!isRevisedNiceString("uurcxstgmygtbstg"));
+    assert.ok(!isRevisedNiceString("ieodomkazucvgmuy"));
+  });
+});
+
+describe("getTotalNiceStringsCountPartTwo", () => {
+  it("counts the total number of nice strings from input", async () => {
+    const numNiceStrings = await getTotalNiceStringsCountPartTwo();
+
+    assert.equal(numNiceStrings, 55);
   });
 });
