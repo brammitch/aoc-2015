6e5798d552ec81863f8da0545076f81c4103636a
Brandon Mitchell <brandon.m.mitchell@gmail.com>
2025-11-20 00:47:09 +0000

    Day 3 Part 2

--- src/03.ts
+++ src/03.ts
@@ -1,32 +1,39 @@
 import fs from "node:fs/promises";
 
-export async function getNumberOfHousesVisited(input: string): Promise<number> {
-  const coordinates: [number, number] = [0, 0];
-  const visitedHouses = new Set<string>();
+export function updateCoordinates(
+  coordinates: [number, number],
+  value: string
+) {
+  switch (value) {
+    // Move north - increment Y-axis
+    case "^":
+      coordinates[1]++;
+      break;
+    // Move east - increment X-axis
+    case ">":
+      coordinates[0]++;
+      break;
+    // Move south - decrement Y-axis
+    case "v":
+      coordinates[1]--;
+      break;
+    // Move west - decrement X-axis
+    case "<":
+      coordinates[0]--;
+      break;
+    default:
+      break;
+  }
+}
 
-  console.log(input.length);
+export async function getNumberOfHousesSantaVisited(
+  input: string
+): Promise<number> {
+  const coordinates: [number, number] = [0, 0];
+  const visitedHouses = new Set<string>(["0, 0"]);
 
   [...input].forEach((value) => {
-    switch (value) {
-      // Move north - increment Y-axis
-      case "^":
-        coordinates[1]++;
-        break;
-      // Move east - increment X-axis
-      case ">":
-        coordinates[0]++;
-        break;
-      // Move south - decrement Y-axis
-      case "v":
-        coordinates[1]--;
-        break;
-      // Move west - decrement X-axis
-      case "<":
-        coordinates[0]--;
-        break;
-      default:
-        break;
-    }
+    updateCoordinates(coordinates, value);
     visitedHouses.add(`${coordinates[0]}, ${coordinates[1]}`);
   });
 
@@ -34,6 +41,34 @@ export async function getNumberOfHousesVisited(input: string): Promise<number> {
 }
 
 const input = await fs.readFile("./src/input/03.txt", "utf-8");
-const numberOfHousesVisited = await getNumberOfHousesVisited(input);
+const numberOfHousesSantaVisited = await getNumberOfHousesSantaVisited(input);
 
-console.log(numberOfHousesVisited);
+console.log(`Santa visited ${numberOfHousesSantaVisited} houses.`);
+
+export async function getNumberOfHousesSantaAndRoboSantaVisited(
+  input: string
+): Promise<number> {
+  const santaCoordinates: [number, number] = [0, 0];
+  const robotCoordinates: [number, number] = [0, 0];
+  const visitedHouses = new Set<string>(["0, 0"]);
+
+  [...input].forEach((value, i) => {
+    // Santa and Robo-Santa take turns moving; Santa goes first.
+    if (i % 2) {
+      updateCoordinates(robotCoordinates, value);
+      visitedHouses.add(`${robotCoordinates[0]}, ${robotCoordinates[1]}`);
+    } else {
+      updateCoordinates(santaCoordinates, value);
+      visitedHouses.add(`${santaCoordinates[0]}, ${santaCoordinates[1]}`);
+    }
+  });
+
+  return visitedHouses.size;
+}
+
+const numberOfHousesSantaAndRoboSantaVisited =
+  await getNumberOfHousesSantaAndRoboSantaVisited(input);
+
+console.log(
+  `Santa and Robo-Santa combined to visit ${numberOfHousesSantaAndRoboSantaVisited} houses.`
+);
--- src/__test__/03.test.ts
+++ src/__test__/03.test.ts
@@ -1,27 +1,65 @@
 import assert from "node:assert";
 import fs from "node:fs/promises";
 import { describe, it } from "node:test";
-import { getNumberOfHousesVisited } from "../03.js";
+import {
+  getNumberOfHousesSantaAndRoboSantaVisited,
+  getNumberOfHousesSantaVisited,
+  updateCoordinates,
+} from "../03.js";
 
-describe("getNumberOfHousesVisited", () => {
+describe("updateCoordinates", () => {
+  it("updates the array in place", () => {
+    const arr: [number, number] = [0, 0];
+    updateCoordinates(arr, "^");
+
+    assert.equal(arr[0], 0);
+    assert.equal(arr[1], 1);
+  });
+});
+
+describe("getNumberOfHousesSantaVisited", () => {
   it("counts the initial house", async () => {
-    const count = await getNumberOfHousesVisited(">");
-    assert.equal(count, 1);
+    const count = await getNumberOfHousesSantaVisited(">");
+
+    assert.equal(count, 2);
   });
 
   it("works with a square pattern", async () => {
-    const count = await getNumberOfHousesVisited("^>v<");
+    const count = await getNumberOfHousesSantaVisited("^>v<");
+
     assert.equal(count, 4);
   });
 
   it("works with a repeating pattern", async () => {
-    const count = await getNumberOfHousesVisited("^v^v^v^v^v");
+    const count = await getNumberOfHousesSantaVisited("^v^v^v^v^v");
+
     assert.equal(count, 2);
   });
 
   it("returns the correct answer for the problem", async () => {
     const input = await fs.readFile("./src/input/03.txt", "utf-8");
-    const count = await getNumberOfHousesVisited(input);
+    const count = await getNumberOfHousesSantaVisited(input);
+
     assert.equal(count, 2565);
+  });
+});
+
+describe("getNumberOfHousesSantaAndRoboSantaVisited", () => {
+  it("counts the initial house", async () => {
+    const count = await getNumberOfHousesSantaAndRoboSantaVisited("^v");
+
+    assert.equal(count, 3);
+  });
+
+  it("works with a square pattern", async () => {
+    const count = await getNumberOfHousesSantaAndRoboSantaVisited("^>v<");
+
+    assert.equal(count, 3);
+  });
+
+  it("works with a repeating pattern", async () => {
+    const count = await getNumberOfHousesSantaAndRoboSantaVisited("^v^v^v^v^v");
+
+    assert.equal(count, 11);
   });
 });
